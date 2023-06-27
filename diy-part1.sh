52e937d80f4f8eb3b33d90eac9dfde75818d5a84
FanxJK <fanxjk@gmail.com>
2023-06-27 23:05:03 +0800

    move to feed

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git fanx https://github.com/FanxJK/openwrt-packages' >>feeds.conf.default
--- files/luci-app-easyupdate/README.md
+++
@@ -1,4 +0,0 @@
-
-# luci-app-easyupdate（简易更新）
-
-基于 https://github.com/sundaqiang/openwrt-packages/tree/master/luci-app-easyupdate 对本项目进行了修改
