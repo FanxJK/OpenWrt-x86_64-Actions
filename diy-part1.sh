a2c80965f8f902388e64606100cdf948b2a56e22
FanxJK <fanxjk@gmail.com>
2024-03-02 18:09:51 +0800

    Remove git revision
    Signed-off-by: FanxJK <fanxjk@gmail.com>

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,6 +13,8 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+# Remove git revision
+sed -i 's/\^.*//g' feeds.conf.default
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
