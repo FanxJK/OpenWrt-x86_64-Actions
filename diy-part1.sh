cdb9b137ff32799a6a50281c1970004f99003712
Fanx <fanxjk@gmail.com>
2024-03-02 18:23:03 +0800

    Remove git revision
    Signed-off-by: Fanx <fanxjk@gmail.com>

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Remove git revision
-sed -i 's/\^.*//g' feeds.conf.default
+curl https://raw.githubusercontent.com/immortalwrt/immortalwrt/openwrt-23.05/feeds.conf.default -o feeds.conf.default
+
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
