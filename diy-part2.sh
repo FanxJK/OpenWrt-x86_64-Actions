f91ba8aac6c4ef33db58691bfc8e56620e5ef7a2
Fanx <22773968+FanxJK@users.noreply.github.com>
2026-07-06 16:32:54 +0800

    fix: use latest OpenClash during firmware build
    - Replace the release-feed luci-app-openclash package with upstream OpenClash in diy-part2
    - Keep the existing .config selection unchanged
    - Update changelog wording for ImmortalWrt 25.12.1

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 #
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
@@ -12,6 +13,18 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+
+# Replace stale luci-app-openclash from ImmortalWrt's release luci feed
+# with the latest upstream OpenClash package. Keep .config unchanged:
+# CONFIG_PACKAGE_luci-app-openclash=y will select this replacement package.
+tmp_openclash="$(mktemp -d)"
+trap 'rm -rf "$tmp_openclash"' EXIT
+
+git clone --depth=1 --filter=blob:none --sparse https://github.com/vernesong/OpenClash.git "$tmp_openclash"
+git -C "$tmp_openclash" sparse-checkout set luci-app-openclash
+
+rm -rf feeds/luci/applications/luci-app-openclash
+mv "$tmp_openclash/luci-app-openclash" feeds/luci/applications/luci-app-openclash
 
 # Disable downloading ci-llvm
 # sed -i 's/--set=llvm\.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/' feeds/packages/lang/rust/Makefile
