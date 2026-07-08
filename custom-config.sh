a378d51c27460475e44f85abca0107697d1cca70
Fanx <22773968+FanxJK@users.noreply.github.com>
2026-07-08 10:46:13 +0800

    fix: refresh OpenClash package during build
    Restore the build-time replacement of luci-app-openclash with the latest
    upstream OpenClash package while keeping the separate sysupgrade keep
    scope override so x86 config backups stay small enough for the boot
    partition.

--- custom-config.sh
+++ custom-config.sh
@@ -14,5 +14,18 @@ set -e
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
+# Replace stale luci-app-openclash from ImmortalWrt's release luci feed
+# with the latest upstream OpenClash package. The sysupgrade keep scope is
+# constrained separately by files/etc/uci-defaults/zzzz-openclash-sysupgrade-keep
+# to avoid backing up large OpenClash runtime/cache files on x86.
+tmp_openclash="$(mktemp -d)"
+trap 'rm -rf "$tmp_openclash"' EXIT
+
+git clone --depth=1 --filter=blob:none --sparse https://github.com/vernesong/OpenClash.git "$tmp_openclash"
+git -C "$tmp_openclash" sparse-checkout set luci-app-openclash
+
+rm -rf feeds/luci/applications/luci-app-openclash
+mv "$tmp_openclash/luci-app-openclash" feeds/luci/applications/luci-app-openclash
+
 # Disable downloading ci-llvm
 # sed -i 's/--set=llvm\.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/' feeds/packages/lang/rust/Makefile
