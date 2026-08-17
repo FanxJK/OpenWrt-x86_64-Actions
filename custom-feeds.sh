18fd6999b34a39a1b0a468f0f5a46c339768ce4f
Fanx <22773968+FanxJK@users.noreply.github.com>
2026-08-17 13:07:39 +0800

    chore: stop overriding the SmartDNS package
    Remove the custom SmartDNS release pin and downloaded package recipe so the firmware build uses the version supplied by the configured feeds.
    
    Signed-off-by: Fanx <22773968+FanxJK@users.noreply.github.com>

--- custom-feeds.sh
+++ custom-feeds.sh
@@ -14,23 +14,6 @@ set -e
 # Remove git revision
 curl https://raw.githubusercontent.com/immortalwrt/immortalwrt/openwrt-25.12/feeds.conf.default -o feeds.conf.default
 
-# Pin SmartDNS to Release48.4
-SMARTDNS_VERSION="48.4"
-SMARTDNS_SOURCE_HASH="b07abba99e921f262ba47588e12f5b36dd849f79c0145542440eb6b7eed80553"
-SMARTDNS_RECIPE_COMMIT="69f474e5d5f764d0f6d4be9e0356f656cf375ba6"
-SMARTDNS_PACKAGE_DIR="package/net/smartdns"
-
-mkdir -p "$SMARTDNS_PACKAGE_DIR"
-curl -fsSL "https://raw.githubusercontent.com/immortalwrt/packages/${SMARTDNS_RECIPE_COMMIT}/net/smartdns/Makefile" -o "$SMARTDNS_PACKAGE_DIR/Makefile"
-
-sed -i \
-  -e "s/^PKG_VERSION:=.*/PKG_VERSION:=${SMARTDNS_VERSION}/" \
-  -e 's/^PKG_RELEASE:=.*/PKG_RELEASE:=1/' \
-  -e "s/^PKG_HASH:=.*/PKG_HASH:=${SMARTDNS_SOURCE_HASH}/" \
-  -e 's/^  DEPENDS:=+i386:libatomic +libopenssl$/  DEPENDS:=+i386:libatomic +libopenssl +zlib/' \
-  -e "s|^include ../../lang/rust/rust-package.mk$|include \$(TOPDIR)/feeds/packages/lang/rust/rust-package.mk|" \
-  "$SMARTDNS_PACKAGE_DIR/Makefile"
-
 # Add custom package feed
 echo 'src-git-full fanx https://github.com/FanxJK/openwrt-packages' >> feeds.conf.default
 
